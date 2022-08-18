ff9ebe06d64a66f76c55a7acf72172e3863f77d0
brandon <12287821-lickitungxbt@users.noreply.gitlab.com>
2022-08-18 14:32:19 -0500

    Quick fix

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -4,8 +4,9 @@ import { useAppSelector } from 'state/hooks'
 import sortByListPriority from 'utils/listSort'
 
 import BROKEN_LIST from '../../constants/tokenLists/broken.tokenlist.json'
-import { UNSUPPORTED_LIST_URLS } from './../../constants/lists'
+import UNSUPPORTED_TOKEN_LIST from '../../constants/tokenLists/broken.tokenlist.json'
 import { AppState } from '../index'
+import { UNSUPPORTED_LIST_URLS } from './../../constants/lists'
 
 export type TokenAddressMap = ChainTokenMap
 
@@ -93,13 +94,16 @@ export function useUnsupportedTokenList(): TokenAddressMap {
   // get hard-coded broken tokens
   const brokenListMap = useMemo(() => tokensToChainTokenMap(BROKEN_LIST), [])
 
+  // get hard-coded list of unsupported tokens
+  const localUnsupportedListMap = useMemo(() => tokensToChainTokenMap(UNSUPPORTED_TOKEN_LIST), [])
+
   // get dynamic list of unsupported tokens
   const loadedUnsupportedListMap = useCombinedTokenMapFromUrls(UNSUPPORTED_LIST_URLS)
 
   // format into one token address map
   return useMemo(
-    () => combineMaps(brokenListMap, loadedUnsupportedListMap),
-    [brokenListMap, loadedUnsupportedListMap]
+    () => combineMaps(brokenListMap, combineMaps(localUnsupportedListMap, loadedUnsupportedListMap)),
+    [brokenListMap, localUnsupportedListMap, loadedUnsupportedListMap]
   )
 }
 export function useIsListActive(url: string): boolean {
